68e224c55e01ffb429bc762e07dc018720cdf101
TravikSkoot <einsphil@gmail.com>
2026-06-06 08:10:32 +0200

    refactor: update item properties

--- src/main/java/de/travikskoot/magicconchshell/item/MagicConchShellItems.java
+++ src/main/java/de/travikskoot/magicconchshell/item/MagicConchShellItems.java
@@ -3,7 +3,6 @@ package de.travikskoot.magicconchshell.item;
 import de.travikskoot.magicconchshell.MagicConchShell;
 import de.travikskoot.magicconchshell.item.custom.MagicConchShellItem;
 import de.travikskoot.magicconchshell.item.custom.MagicConchShellWorldsSmallestViolinItem;
-import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -12,12 +11,12 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
 
+import java.util.List;
 import java.util.function.Function;
 
 import static net.minecraft.world.item.component.Consumables.defaultFood;
@@ -28,23 +27,47 @@ public class MagicConchShellItems {
             properties -> new MagicConchShellItem(properties
                     .rarity(Rarity.EPIC)
                     .stacksTo(1)
-                    .useCooldown(1.0F)));
+                    .useCooldown(1.0F)
+                    .durability(250)
+            ));
     public static final Item KRABBY_PATTY = registerItem("krabby_patty",
             properties -> new Item(properties
-                    .food((new FoodProperties.Builder()).nutrition(8).saturationModifier(0.8F).build())));
+                    .food((new FoodProperties.Builder())
+                            .nutrition(10)
+                            .saturationModifier(1.0F)
+                            .build())
+            ));
     public static final Item NASTY_PATTY = registerItem("nasty_patty",
-            properties -> new Item(properties.food(((new FoodProperties.Builder()).nutrition(4).saturationModifier(0.1F).build()), defaultFood().onConsume(new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(MobEffects.HUNGER, 600, 0), 0.8F)).build())));
+            properties -> new Item(properties
+                    .food(((new FoodProperties.Builder())
+                            .nutrition(4)
+                            .saturationModifier(0.1F)
+                            .build()),
+
+                            defaultFood()
+                            .onConsume(
+                                    new ApplyStatusEffectsConsumeEffect(
+                                            List.of(
+                                                    new MobEffectInstance(MobEffects.POISON, 1200, 1), new MobEffectInstance(MobEffects.HUNGER, 300, 2), new MobEffectInstance(MobEffects.NAUSEA, 300, 0)
+                                            )
+                                    )
+                            )
+                    .build())
+            ));
     public static final Item SPATULA = registerItem("spatula",
             properties -> new Item(properties
                     .rarity(Rarity.RARE)
-                    .sword(ToolMaterial.IRON, 3.0F, -2.4F)));
+                    .sword(ToolMaterial.IRON, 3.0F, -2.4F)
+            ));
     public static final Item WORLDS_SMALLEST_VIOLIN = registerItem("worlds_smallest_violin",
             properties -> new MagicConchShellWorldsSmallestViolinItem(properties
                     .rarity(Rarity.RARE)
-                    .stacksTo(1)));
+                    .stacksTo(1)
+            ));
     public static final Item SECRET_FORMULA = registerItem("secret_formula",
             properties -> new Item(properties
-                    .rarity(Rarity.RARE)));
+                    .rarity(Rarity.RARE)
+            ));
 
     private static Item registerItem(String name, Function<Item.Properties, Item> function) {
         return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(MagicConchShell.MOD_ID, name),
