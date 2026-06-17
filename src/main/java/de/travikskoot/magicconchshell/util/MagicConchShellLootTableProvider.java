e233ac1b4580e50998ee38884681fd2baef21bd4
TravikSkoot <thiemo.kuepper@gmail.com>
2026-06-17 19:08:03 +0200

    Fix enchantment multiplier

--- src/main/java/de/travikskoot/magicconchshell/util/MagicConchShellLootTableProvider.java
+++ src/main/java/de/travikskoot/magicconchshell/util/MagicConchShellLootTableProvider.java
@@ -14,6 +14,7 @@ import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.EnchantedCountIncreaseFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemEntityPropertyCondition;
 import net.minecraft.world.level.storage.loot.predicates.LootItemKilledByPlayerCondition;
@@ -43,7 +44,6 @@ public class MagicConchShellLootTableProvider {
             ResourceKey.create(Registries.LOOT_TABLE, Identifier.withDefaultNamespace("entities/zombie_villager"));
     private static final ResourceKey<LootTable> ZOMBIFIED_PIGLIN =
             ResourceKey.create(Registries.LOOT_TABLE, Identifier.withDefaultNamespace("entities/zombified_piglin"));
-
     private static final ResourceKey<LootTable> NAUTILUS =
             ResourceKey.create(Registries.LOOT_TABLE, Identifier.withDefaultNamespace("entities/nautilus"));
 
@@ -114,6 +114,8 @@ public class MagicConchShellLootTableProvider {
                                 .add(
                                         LootItem.lootTableItem(MagicConchShellItems.NASTY_PATTY)
                                                 .apply(SetItemCountFunction.setCount(UniformGenerator.between(0.0F, 2.0F)))
+                                                .apply(EnchantedCountIncreaseFunction.lootingMultiplier(registries, UniformGenerator.between(0.0F, 1.0F)))
+
                                 )
                                 .when(LootItemKilledByPlayerCondition.killedByPlayer())
                                 .when(
